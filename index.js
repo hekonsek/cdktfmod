775ebc0c4a39b9418a7597356dbb24897056aa3b
Henryk Konsek <hekonsek@gmail.com>
2022-01-17 16:22:30 +0100

    Init commit.

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.Project = void 0;
+var Project = /** @class */ (function () {
+    function Project() {
+    }
+    return Project;
+}());
+exports.Project = Project;
